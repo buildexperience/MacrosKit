5fc548066843e106e64258fd0d303e159e2a4eb8
Joe Maghzal <joemaghzal@icloud.com>
2025-07-24 11:42:40 +0300

    Add helper methods for diagnostics

--- Sources/MacrosKit/MacroErrorFixItMessage.swift
+++ Sources/MacrosKit/MacroErrorFixItMessage.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftSyntax
 import SwiftDiagnostics
 
 /// A message for fix-its associated with macro errors.
@@ -35,5 +36,46 @@ public struct MacroFixItMessage: FixItMessage {
     public init(message: String, domain: String, id: String) {
         self.message = message
         self.fixItID = MessageID(domain: domain, id: id)
+    }
+}
+
+extension Diagnostic {
+    // Wraps the diagnostic in a `DiagnosticsError`.
+    ///
+    /// Use this computed property to convert a single `Diagnostic`
+    /// into an error that can be thrown or propagated.
+    public var error: DiagnosticsError {
+        return DiagnosticsError(diagnostics: [self])
+    }
+    
+    /// Returns a copy of the diagnostic with an additional fix-it suggestion.
+    ///
+    /// This method appends a new `FixIt` to the diagnostic’s existing fix-its.
+    /// The new fix-it is created by applying the provided closure to the current diagnostic.
+    ///
+    /// - Parameter fixIt: A closure that takes the diagnostic as input and returns a `FixIt`.
+    /// - Returns: A new `Diagnostic` instance with the added fix-it.
+    public func fixIt(_ fixIt: (_ diag: Diagnostic) -> FixIt) -> Self {
+        return Diagnostic(
+            node: node,
+            position: position,
+            message: diagMessage,
+            highlights: highlights,
+            notes: notes,
+            fixIts: fixIts + [fixIt(self)]
+        )
+    }
+}
+
+extension DiagnosticMessage {
+    /// Creates a diagnostic at the specified syntax node.
+    ///
+    /// This convenience method produces a `Diagnostic` using the receiver
+    /// as the diagnostic message, and attaches it to the given syntax node.
+    ///
+    /// - Parameter node: The syntax node at which the diagnostic should be reported.
+    /// - Returns: A `Diagnostic` with this message at the given node.
+    public func diagnose(at node: some SyntaxProtocol) -> Diagnostic {
+        Diagnostic(node: node, message: self)
     }
 }
